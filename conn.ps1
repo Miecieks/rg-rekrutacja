913a191dcdde80a7f54921afafa0918fd626f010
Mateusz-Muzyka <131866876+Miecieks@users.noreply.github.com>
2026-07-14 00:15:06 +0200

    Update conn.ps1

--- conn.ps1
+++ conn.ps1
@@ -4,10 +4,6 @@
 $secure_secret = ConvertTo-SecureString $client_secret -AsPlainText -Force
 $Credential = New-Object -TypeName "System.Management.Automation.PSCredential" -ArgumentList $client_id, $secure_secret
 
-
-
 Connect-AzAccount -ServicePrincipal -TenantId $tenant_id -Credential $Credential | Out-Null
-
-
 
 New-AzStorageAccount -ResourceGroupName $resource_group -Name "rekrutacja" -Location $location -SkuName $skuname -kind $kind
