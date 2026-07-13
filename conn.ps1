aecc7744a8c33a2f1380e26ce6c1dbaed5bdc76e
Mateusz-Muzyka <131866876+Miecieks@users.noreply.github.com>
2026-07-14 00:13:32 +0200

    Skrypt powershell
    skrypt tworzący azure storage account

---
+++ conn.ps1
@@ -0,0 +1,13 @@
+.".\vars.ps1"
+
+
+$secure_secret = ConvertTo-SecureString $client_secret -AsPlainText -Force
+$Credential = New-Object -TypeName "System.Management.Automation.PSCredential" -ArgumentList $client_id, $secure_secret
+
+
+
+Connect-AzAccount -ServicePrincipal -TenantId $tenant_id -Credential $Credential | Out-Null
+
+
+
+New-AzStorageAccount -ResourceGroupName $resource_group -Name "rekrutacja" -Location $location -SkuName $skuname -kind $kind
