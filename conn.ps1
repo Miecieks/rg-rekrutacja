835090dea34cd0d98f8e2006f64968585dc00d79
Mateusz-Muzyka <131866876+Miecieks@users.noreply.github.com>
2026-07-14 00:16:02 +0200

    finalna wersja
    usunięto zbędne linie

--- conn.ps1
+++ conn.ps1
@@ -1,6 +1,5 @@
 .".\vars.ps1"
 
-
 $secure_secret = ConvertTo-SecureString $client_secret -AsPlainText -Force
 $Credential = New-Object -TypeName "System.Management.Automation.PSCredential" -ArgumentList $client_id, $secure_secret
 
